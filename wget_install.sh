81a2353b4ca846a96771990ac773f8d1bd799a62
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-02-03 13:59:12 +0100

    Update wget_install.sh

--- wget_install.sh
+++ wget_install.sh
@@ -76,10 +76,10 @@ Naciśnij dowolny klawisz aby kontynuować ...
 lub CTRL-C aby anulować"
 read -n 1 -s -r -p  ""
 
-
+clear
 echo -e "
 ------------------------
-za chwilę nastąpi instalacja niezbędnych składników
+Za chwilę nastąpi instalacja niezbędnych składników
 ------------------------
 "
 for i in `seq 1 9`;
@@ -93,7 +93,6 @@ for i in `seq 1 9`;
 
 sudo apt-get update
 packages=$(printf "
-git
 lynx
 syslinux-common
 syslinux-efi
@@ -101,6 +100,7 @@ isc-dhcp-server
 tftpd-hpa
 pxelinux
 network-manager
+rsync
 gcp
 lighttpd
 nfs-kernel-server")
@@ -108,15 +108,69 @@ nfs-kernel-server")
 
 sudo apt-get -y install $packages
 
+echo -e "
+------------------------
+Ustawienie DHCP
+------------------------
+"
 bash ./script/dhcp.sh
+echo -e "
+------------------------
+Ustawienie TFTP
+------------------------
+"
 bash ./script/tftp.sh
+echo -e "
+------------------------
+Kopiowanie niezbędnych plików
+------------------------
+"
 bash ./script/copy.sh
+echo -e "
+------------------------
+Pobieranie obrazu ISO
+------------------------
+"
 bash ./script/debian_install.sh
 
 
 
 
 
+echo -e "
+------------------------
+Pobieranie obrazu live ISO
+------------------------
+"
+
+echo ""
+echo ""
+echo "Czy chcesz pobrać obraz Live Debian " 
+echo "i dodać go do serwera PXE? [y/N] "
+
+
+
+while read -r -p " " response
+do
+if [[ "$response" =~ ^([yY][eE][sS]|[yY]|[tT])$ ]]
+then
+	bash ./script/debian_live.sh
+	
+	echo "Czy chcesz pobrać inny obraz  [y/N]"
+	continue
+
+else
+
+	echo "Dziękuję!"
+fi
+break
+done
+
+echo -e "
+------------------------
+Ustawienia karty sieciowej
+------------------------
+"
 echo -e "
 
 Pamiętaj o ustawieniach karty sieciowej
@@ -130,29 +184,18 @@ oraz należy ustalić na jakim porcie ma być nasłuchiwanie"
 
 
 bash ./script/nmcli.sh
-bash ./script/isc.sh
 
 
 
-echo ""
-echo ""
-echo "Czy chcesz pobrać obraz Live Debian " 
-echo "i dodać go do serwera PXE? [y/N] "
-read -r -p " " response
 
 
-if [[ "$response" =~ ^([yY][eE][sS]|[yY]|[tT])$ ]]
-then
-	bash ./script/debian_live.sh
-else
-	echo "Dziękuję!"
-fi
 echo ""
 echo "------------------------------------------"
 echo ""
-read -p "Naciśnij [Enter] aby zakończyć..."
+echo "Naciśnij [Enter] aby zakończyć..."
 echo ""
 echo "------------------------------------------"
+read -p ""
 sudo systemctl restart isc-dhcp-server.service 
 
 
@@ -164,6 +207,8 @@ log skryptu zapisany w /home/$USER/log.txt
 by edytować menu można zkożystać z :
 /srv/tftp/uefi_menu_edit
 oraz
-/srv/tftp/bios_menu_edit"
+/srv/tftp/bios_menu_edit
+"
+sudo systemctl restart lighttpd.service 
 sleep 3
 sudo systemctl restart tftpd-hpa.service 
