de0b88b0652f79dbfc63b27bc17b0207bf306da6
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-04-26 14:05:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,35 +5,36 @@ exec > >(tee /home/$USER/log.txt)
 
 clear
 echo -e "
-Skrypt ustawiający server PXE 
-Dla BIOS oraz UEFI, Obraz instalacji Debian 11
-Przed uruchomieniem należy zmienić ustawienia karty sieciowej na adres statyczny
-Nalezy ustawić adres:
+PXE server setup script for Legacy and UEFI, 
+It set default Debian 11 netinstall image
+
+Before starting, change the network adapter settings to a static address
+You need to set the address:
 
 192.168.0.2
 255.255.255.0
 192.168.0.0
 
-oraz należy ustalić na jakim porcie ma być nasłuchiwanie
+and you need to determine on which port you want to listen
 
-Instalacja potrzebnych składników
-oraz uruchmienie skryptu
+Installing the necessary components
+and running the script
 
-Skryp pozwoli również na ustawienie adresu IP
-w dalszej cześci programu
+The script will also allow you to set the IP address
+later in the program
 
-Naciśnij dowolny klawisz aby kontynuować ...
-lub CTRL-C aby anulować"
+Press any key to continue ...
+or CTRL-C to cancel"
 read -n 1 -s -r -p  ""
 
 clear
 echo -e "
 ------------------------
-Za chwilę nastąpi instalacja niezbędnych składników
+Installing req. in a moment...
 ------------------------
 "
 printf "
-----Niezbędne----	----Dodatkowe----
+----Requirment----	----Extra----
 syslinux-common		lynx
 syslinux-efi		network-manager
 isc-dhcp-server		rsync
@@ -73,26 +74,26 @@ sudo apt-get -y install $packages
 
 echo -e "
 ------------------------
-Ustawienie DHCP
+DHCP Setting script
 ------------------------
 "
 bash ./script/dhcp.sh
 echo -e "
 ------------------------
-Ustawienie TFTP
+TFTP Setting script
 ------------------------
 "
 bash ./script/tftp.sh
 echo -e "
 ------------------------
-Kopiowanie niezbędnych plików
+Copying necassary files
 ------------------------
 "
 bash ./script/copy.sh
 
 echo -e "
 ------------------------
-Pobieranie obrazu ISO
+Downloading ISO
 ------------------------
 "
 bash ./script/debian_install.sh
@@ -100,14 +101,14 @@ bash ./script/debian_install.sh
 
 echo -e "
 ------------------------
-Pobieranie obrazu live ISO
+Downloading live ISO
 ------------------------
 "
 
 echo ""
 echo ""
-echo "Czy chcesz pobrać obraz Live Debian " 
-echo "i dodać go do serwera PXE? [y/N] "
+echo "Do you want to download Debian Live ISO " 
+echo "and add it to PXE server? [y/N] "
 
 
 while read -r -p " " debiso
@@ -116,31 +117,31 @@ if [[ "$debiso" =~ ^([yY][eE][sS]|[yY]|[tT])$ ]]
 then
 	bash ./script/debian_live.sh
 	
-	echo "Czy chcesz pobrać inny obraz  [y/N]"
+	echo "Do you want to download other ISO?  [y/N]"
 	continue
 
 else
 
-	echo "Dziękuję!"
+	echo "Thank you!"
 fi
 break
 done
 
 echo -e "
 ------------------------
-Ustawienia karty sieciowej
+Network Card Settings
 ------------------------
 "
 echo -e "
 
-Pamiętaj o ustawieniach karty sieciowej
-Nalezy ustawić adres:
+Remember your network settings
+Address must be set:
 
 192.268.0.2
 255.255.255.0
 192.168.0.0
 
-oraz należy ustalić na jakim porcie ma być nasłuchiwanie"
+and you need to determine on which port you want to listen"
 
 
 de=$(echo $DESKTOP_SESSION)
@@ -157,7 +158,7 @@ fi
 echo ""
 echo "------------------------------------------"
 echo ""
-echo "Naciśnij [Enter] aby zakończyć..."
+echo "Press [Enter] to quit..."
 echo ""
 echo "------------------------------------------"
 read -p ""
@@ -167,11 +168,11 @@ sudo systemctl restart isc-dhcp-server.service
 
 printf "
 
-log skryptu zapisany w /home/$USER/log.txt
+script log saved in /home/$USER/log.txt
 
-by edytować menu można zkożystać z :
+To edit PXE menu you can use :
 /srv/tftp/uefi_menu_edit
-oraz
+and
 /srv/tftp/bios_menu_edit
 "
 sudo systemctl restart lighttpd.service 
